2415f083c1a367a06b178f8d709c1009f01e8a85
Krzysztof Gertig <ktgertig@gmail.com>
2018-03-04 13:08:01 +0000

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
 $(function(){
 	//this code will execute after the DOM is loaded
 });
+var carouselList = $('#carousel ul');
+
+setInterval(changeSlide, 300);
+
+function changeSlide() {
+	carouselList.animate({'marginLeft':'-=400px'}, 500);
+}
